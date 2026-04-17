6e6bdf583719db15d043a65c17a92b6bb8ceb834
Phy Vathanak <vathanakphy@gmail.com>
2026-04-17 16:52:56 +0700

    return bike (#22)

--- lib/data/repositories/bookings/bookings_repository.dart
+++ lib/data/repositories/bookings/bookings_repository.dart
@@ -3,4 +3,8 @@ import 'package:velo_pp/model/booking.dart';
 
 abstract class BookingsRepository extends ChangeNotifier {
   List<Booking> getBookingsForUser(String userId);
+  Future<void> completeBooking({
+    required String bookingId,
+    required String endStationId,
+  });
 }
